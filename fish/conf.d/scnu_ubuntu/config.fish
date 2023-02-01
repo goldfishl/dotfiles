c18383139b646be58fd482755a8e0ae12796b35c
goldfishl <sicnu.long@gmail.com>
2023-02-01 16:35:04 +0800

    cancel `conda` and enable default venv in scnu_ubuntu

--- fish/conf.d/scnu_ubuntu/config.fish
+++ fish/conf.d/scnu_ubuntu/config.fish
@@ -1,12 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    # >>> conda initialize >>>
-    # !! Contents within this block are managed by 'conda init' !!
-    eval /opt/conda/bin/conda "shell.fish" "hook" $argv | source
-    # <<< conda initialize <<<
-
-    # others
+    # tools
     ## autojump
     [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    #eval /opt/conda/bin/conda "shell.fish" "hook" $argv | source
+    # <<< conda initialize <<<
+
+    # venv default python env
+    source (autojump bin)/activate.fish	
+
 end
