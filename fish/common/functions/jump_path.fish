72ef87c0b34f0a177abdd9afba3d02cf7065bd13
goldfishl <sicnu.long@gmail.com>
2023-02-07 03:50:00 +0800

    bug fix

--- fish/common/functions/jump_path.fish
+++ fish/common/functions/jump_path.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 function jump_path -a _dir_name
-	set -lx dot_path (autojump $_dir_name)
+	set -lx dot_path (autojump $_dir_name) && test ! $dot_path = "."
 	or read -P "autojump fail to find `$_dir_name` directory, you need manually input the directory:" -lx dot_path
 	or return
 	if not test -d $dot_path	
