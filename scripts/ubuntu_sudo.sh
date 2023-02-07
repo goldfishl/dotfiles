0579bb9814efe72910e6dd86d01fbc97557750aa
goldfishl <sicnu.long@gmail.com>
2023-02-08 00:26:41 +0800

    fix

---
+++ scripts/ubuntu_sudo.sh
@@ -0,0 +1,6 @@
+#/usr/bin/env bash
+
+find . -type f | grep -v ubuntu_sudo.fish
+find . -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/add-apt-repository/sudo add-apt-repository/g"
+find . -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/apt-add-repository/sudo apt-add-repository/g"
+find . -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/apt/sudo apt/g"
