1a74c85e39f416ca8f9500a78d6f7760609d98dd
goldfishl <sicnu.long@gmail.com>
2023-02-08 00:38:11 +0800

    fixxx

--- scripts/ubuntu_sudo.sh
+++ scripts/ubuntu_sudo.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 
-find . -type f | grep -v ubuntu_sudo.fish
-find . -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/add-apt-repository/sudo add-apt-repository/g"
-find . -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/apt-add-repository/sudo apt-add-repository/g"
-find . -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/apt/sudo apt/g"
-find . -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/chsh/sudo chsh/g"
+find . -type f | grep -v ubuntu_sudo.sh
+find . -type f | grep -v ubuntu_sudo.sh | xargs sed -i "s/add-apt-repository/sudo add-apt-repository/g"
+find . -type f | grep -v ubuntu_sudo.sh | xargs sed -i "s/apt-add-repository/sudo apt-add-repository/g"
+find . -type f | grep -v ubuntu_sudo.sh | xargs sed -i "s/apt/sudo apt/g"
+find . -type f | grep -v ubuntu_sudo.sh | xargs sed -i "s/chsh/sudo chsh/g"
