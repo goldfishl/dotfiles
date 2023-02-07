4d77dd53c19e889ea0fd731ed9189aedd0094bdd
goldfishl <sicnu.long@gmail.com>
2023-02-08 00:09:23 +0800

    fix

--- scripts/ubuntu_sudo.fish
+++ scripts/ubuntu_sudo.fish
@@ -5,7 +5,7 @@ if set -q _flag_d
     test -d $_flag_d
     or echo "dir not exist." && return 1
     find $_flag_d -type f | grep -v ubuntu_sudo.fish
-    find $_flag_d -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/add-apt-repository/sudo apt-add-repository/g"
+    find $_flag_d -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/add\-apt\-repository/sudo add\-apt\-repository/g"
     find $_flag_d -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/^apt/sudo apt/g"
     echo "change all files to sudo."
 else
