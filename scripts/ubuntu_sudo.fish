d1ec867eb7769f83e9dee915def9f1f6752d15ec
goldfishl <sicnu.long@gmail.com>
2023-02-08 00:11:47 +0800

    fix

--- scripts/ubuntu_sudo.fish
+++ scripts/ubuntu_sudo.fish
@@ -3,11 +3,11 @@
 argparse 'd/dir=' -- $argv
 if set -q _flag_d
     test -d $_flag_d
-    or echo "dir not exist." && return 1
+    or echo "dir not exist." and return 1
     find $_flag_d -type f | grep -v ubuntu_sudo.fish
     find $_flag_d -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/add\-apt\-repository/sudo add\-apt\-repository/g"
     find $_flag_d -type f | grep -v ubuntu_sudo.fish | xargs sed -i "s/^apt/sudo apt/g"
     echo "change all files to sudo."
 else
-    echo "please input directory" && return 1
+    echo "please input directory" and return 1
 end
