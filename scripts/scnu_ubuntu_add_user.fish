ae9470f4cd4e61fbad679805f0d844451620a11f
goldfishl <sicnu.long@gmail.com>
2023-02-08 14:23:44 +0800

    modify scnu user add script

--- scripts/scnu_ubuntu_add_user.fish
+++ scripts/scnu_ubuntu_add_user.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 
-argparse 'n/name' -- $argv
-set -q _flag_n or return 1
+read -P "user name: " -l _flag_n
+or return
 
-useradd -d /ai/ljl/$_flag_n -m $_flag_n -s /bin/bash
+useradd -d /ai/ljl1/$_flag_n -m $_flag_n -s /bin/bash
 passwd $_flag_n
 conda create -n $_flag_n --clone base
-conda init --system bash
-echo "conda activate $_flag_n"
+cat scnu_conda_init.bashrc >> /ai/ljl1/$_flag_n/.bashrc
+echo "conda activate $_flag_n" >> /ai/ljl1/$_flag_n/.bashrc
